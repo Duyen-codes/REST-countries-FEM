4c50a214da71e4b946a5ac2a872a0bdbe5309ff1
Duyen Nguyen <hongduyen0705@gmail.com>
2022-03-18 04:13:20 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,14 @@
+async function fetchAllCountries() {
+  const response = await fetch("https://restcountries.com/v2/all");
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  const countries = await response.json();
+  console.log("from fetch", countries);
+  return countries;
+}
+
+fetchAllCountries().then((countries) => {
+  countries.forEach((country) => console.log(country.flag));
+});
