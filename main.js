0ec1c0c372f76b53e5ad0cdfa07ffaad74bf58a3
Duyen Nguyen <hongduyen0705@gmail.com>
2022-03-18 06:44:17 +0200

    search country by name and render result function done

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+// Toggle theme
 const toggleBtn = document.querySelector(".btn-toggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("light");
 });
 const cards = document.querySelector(".cards");
 
+// Fetch countries
 async function fetchAllCountries() {
   const response = await fetch("https://restcountries.com/v2/all");
   if (!response.ok) {
@@ -11,25 +13,50 @@ async function fetchAllCountries() {
     throw new Error(message);
   }
   const countries = await response.json();
-  console.log("from fetch", countries);
   return countries;
 }
 
+// Render cards
 fetchAllCountries().then((countries) => {
   countries.forEach((country) => {
-    cards.insertAdjacentHTML(
-      "beforeend",
-      `
-    <a href="#" class="card">
-    <img class="flag" src="${country.flag}" alt="" />
-    <div class="content">
-      <h3 class="name">${country.name}</h3>
-      <div><span class="country-info">Population: </span><span class="population">${country.population}</span></div>
-      <div><span class="country-info">Region: </span><span class="region">${country.region}</span></div>
-
-      <div><span class="country-info">Capital: </span><span class="capital">${country.capital}</span></div>
-    </div>
-  </a>`
-    );
+    renderCard(country);
   });
 });
+
+const renderCard = (country) => {
+  cards.innerHTML = "";
+  cards.insertAdjacentHTML(
+    "beforeend",
+    `
+  <a href="#" class="card">
+  <img class="flag" src="${country.flag}" alt="" />
+  <div class="content">
+    <h3 class="name">${country.name}</h3>
+    <div><span class="country-info">Population: </span><span class="population">${country.population}</span></div>
+    <div><span class="country-info">Region: </span><span class="region">${country.region}</span></div>
+
+    <div><span class="country-info">Capital: </span><span class="capital">${country.capital}</span></div>
+  </div>
+</a>`
+  );
+};
+
+// Search
+const searchInput = document.querySelector(".search-input");
+
+const search = function () {
+  console.log("searching", searchInput.value);
+  fetchAllCountries().then((countries) => {
+    countries
+      .filter((country) =>
+        country.name.toLowerCase().includes(searchInput.value.toLowerCase())
+      )
+      .forEach((country) => {
+        console.log("from search", country);
+        renderCard(country);
+      });
+  });
+};
+searchInput.addEventListener("keyup", search);
+
+// Filter
