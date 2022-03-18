d08eb2cd48022b6e0c751f033e9094f7414c9fe6
Duyen Nguyen <hongduyen0705@gmail.com>
2022-03-18 05:52:29 +0200

    fetched data and displayed on page

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const toggleBtn = document.querySelector(".btn-toggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("light");
 });
+const cards = document.querySelector(".cards");
 
 async function fetchAllCountries() {
   const response = await fetch("https://restcountries.com/v2/all");
@@ -14,6 +15,20 @@ async function fetchAllCountries() {
   return countries;
 }
 
-// fetchAllCountries().then((countries) => {
-//   countries.forEach((country) => console.log(country.flag));
-// });
+fetchAllCountries().then((countries) => {
+  countries.forEach((country) => {
+    cards.insertAdjacentHTML(
+      "beforeend",
+      `
+    <a href="#" class="card">
+    <img class="flag" src="${country.flag}" alt="" />
+    <div class="content">
+      <h3 class="name">${country.name}</h3>
+      <p>Population: <span class="population">71234</span></p>
+      <p>Region: <span class="region">${country.region}</span></p>
+      <p>Capital: <span class="capital">${country.capital}</span></p>
+    </div>
+  </a>`
+    );
+  });
+});
